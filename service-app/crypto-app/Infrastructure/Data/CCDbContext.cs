64fa152c393001477f3ed53ae325923cccc3a975
Ayoub <lfatmi.ayoub@gmail.com>
2025-02-04 16:39:57 +0100

    Added migrations

--- service-app/crypto-app/Infrastructure/Data/CCDbContext.cs
+++ service-app/crypto-app/Infrastructure/Data/CCDbContext.cs
@@ -1,12 +1,57 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using crypto_app.Core.Entities;
+using crypto_app.Core.Entities.Users;
 
 namespace crypto_app.Infrastructure.Data
 {
-    public class CCDbContext
+    public class CCDbContext : DbContext
     {
-        
+        public CCDbContext(DbContextOptions<CCDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.HasIndex(e => e.ApplicationAccessCode).IsUnique();
+                entity.Property(e => e.ApplicationAccessCode).IsRequired().HasMaxLength(6);
+                entity.Property(e => e.UserType).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.AppIcon).HasMaxLength(256);
+                entity.Property(e => e.Theme).IsRequired();
+            });
+
+            modelBuilder.Entity<Language>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Code).HasMaxLength(5);
+            });
+
+            modelBuilder.Entity<Currency>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Symbol).IsRequired().HasMaxLength(10);
+                entity.Property(e => e.Code).IsRequired().HasMaxLength(3);
+            });
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.CountryCode).IsRequired().HasMaxLength(3);
+            });
+        }
     }
 }
---
+++ service-app/crypto-app/Migrations/20250204153515_InitialCreate.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crypto_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ApplicationAccessCode = table.Column<string>(type: "TEXT", maxLength: 6, nullable: false),
+                    UserType = table.Column<int>(type: "INTEGER", nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "TEXT", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "TEXT", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "TEXT", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    CountryCode = table.Column<string>(type: "TEXT", maxLength: 3, nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UpdatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Symbol = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    Code = table.Column<string>(type: "TEXT", maxLength: 3, nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UpdatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Code = table.Column<string>(type: "TEXT", maxLength: 5, nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UpdatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BaseUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    AdditionalName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false),
+                    EmailAddress = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    ApplicationUserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    LanguageId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CurrencyId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CountryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 8, nullable: false),
+                    AppIcon = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
+                    Theme = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UpdatedDateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BaseUsers_ApplicationUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "ApplicationUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BaseUsers_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BaseUsers_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BaseUsers_Languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUsers_ApplicationAccessCode",
+                table: "ApplicationUsers",
+                column: "ApplicationAccessCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseUsers_ApplicationUserId",
+                table: "BaseUsers",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseUsers_CountryId",
+                table: "BaseUsers",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseUsers_CurrencyId",
+                table: "BaseUsers",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseUsers_LanguageId",
+                table: "BaseUsers",
+                column: "LanguageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaseUsers");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUsers");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
