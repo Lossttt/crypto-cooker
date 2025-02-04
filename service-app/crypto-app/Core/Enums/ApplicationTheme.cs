185005901fd67fc4765f4dae79f87ad56c9c7ca2
Ayoub <lfatmi.ayoub@gmail.com>
2025-02-04 16:39:03 +0100

    Added default ApplicationTheme enum

---
+++ service-app/crypto-app/Core/Enums/ApplicationTheme.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crypto_app.Core.Enums
+{
+    public enum ApplicationTheme
+    {
+        DarkTheme = 0,
+
+        LightTheme = 1
+    }
+}
