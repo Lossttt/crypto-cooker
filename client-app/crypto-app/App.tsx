ecbf3911c5865aef79f6571bead3b022aef2a942
Ayoub <lfatmi.ayoub@gmail.com>
2025-02-06 15:42:23 +0100

    Added third-party font

--- client-app/crypto-app/App.tsx
+++ client-app/crypto-app/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './navigation/Routes';
+import { loadFonts } from './config/config.fonts';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadAsyncFonts = async () => {
+      await loadFonts();
+      setFontsLoaded(true);
+    };
+
+    loadAsyncFonts();
+  }, []);
+
   return (
     <NavigationContainer>
       <Routes />
