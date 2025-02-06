d4030fae4feaccb3c3a85c0ba8f7946b3f5136e0
Ayoub <lfatmi.ayoub@gmail.com>
2025-02-06 17:26:41 +0100

    Update Signup types

--- client-app/crypto-app/types/user/SignUpTypes.ts
+++ client-app/crypto-app/types/user/SignUpTypes.ts
@@ -1,20 +1,11 @@
 export interface SignUpRequest {
     email: string;
     password: string;
-    confirmPassword: string;
-    username: string;
-    // TO DO: Add more fields here
+    passwordConfirmation: string;
+    phoneNumber: string;
 }
 
 export interface SignUpResponse {
-    success: boolean;
-    message: string;
-    user?: {
-        id: string;
-        username: string;
-        email: string;
-        createdAt: string;
-        updatedAt: string;
-        // TO DO: Add more fields here
-    };
+    userId: string;
+    email: string;
 }
