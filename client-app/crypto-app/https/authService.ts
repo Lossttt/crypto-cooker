7806a6a21e4293e5508a25d6d8c9a15588155cdf
Ayoub <lfatmi.ayoub@gmail.com>
2025-02-07 22:35:59 +0100

    Update authService

--- client-app/crypto-app/https/authService.ts
+++ client-app/crypto-app/https/authService.ts
@@ -2,12 +2,19 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SignInRequest, SignInResponse } from '../types/user/signInTypes';
 import { apiEnvironment } from '../config/apiConfig';
+
 const AUTH_TOKEN_KEY = 'auth_token';
 const REFRESH_TOKEN_KEY = 'refresh_token';
+const API_URL = apiEnvironment.service_app_public_url;
+
+const handleApiError = (error: any) => {
+    const errorMessage = error.response?.data?.message || 'An unexpected error occurred.';
+    throw new Error(errorMessage);
+};
 
 export const authService = {
     async signIn(data: SignInRequest): Promise<SignInResponse> {
-        const response = await axios.post<SignInResponse>(`${apiEnvironment.service_app_public_url}/users/authenticate`, data);
+        const response = await axios.post<SignInResponse>(`${API_URL}/users/authenticate`, data);
         if (response.data.accessToken && response.data.refreshToken) {
             await this.saveTokens(response.data.accessToken.token, response.data.refreshToken);
         } else {
@@ -32,5 +39,32 @@ export const authService = {
     async clearTokens() {
         await AsyncStorage.removeItem(AUTH_TOKEN_KEY);
         await AsyncStorage.removeItem(REFRESH_TOKEN_KEY);
+    },
+
+    async resetPassword(email: string) {
+        try {
+            const response = await axios.post(`${API_URL}/users/reset-password/request`, { email });
+            return response.data;
+        } catch (error) {
+            handleApiError(error);
+        }
+    },
+
+    async verifyCode(email: string, code: string) {
+        try {
+            const response = await axios.post(`${API_URL}/users/reset-password/verify`, { email, token: code });
+            return response.data;
+        } catch (error) {
+            handleApiError(error);
+        }
+    },
+
+    async changePassword(email: string, newPassword: string, token: string) {
+        try {
+            const response = await axios.post(`${API_URL}/users/reset-password`, { email, newPassword, token });
+            return response.data;
+        } catch (error) {
+            handleApiError(error);
+        }
     }
 };
